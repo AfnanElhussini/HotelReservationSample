62c6e22a5db1dc0c13976c2fd286ba5bacec424b
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-05-26 13:53:45 +0300

    Update Models and Services

--- hotelapp/src/app/app-routing.module.ts
+++ hotelapp/src/app/app-routing.module.ts
@@ -3,10 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoomListComponent } from './Components/room-resource/room-list/room-list.component';
 import { HomeComponent } from './shared/components/home/home.component';
 import { RoomDetailsComponent } from './Components/room-resource/room-details/room-details.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegistrationComponent } from './account/registration/registration.component';
+
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: "full", },
   { path: "rooms", loadChildren: () => import("./Components/room-resource/room-resource.module").then(m => m.RoomResourceModule) },
+  
+ 
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+
+
+  //Login route
+ 
+
 ];
 
 
