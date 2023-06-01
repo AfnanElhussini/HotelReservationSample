d74d9d64f6a6e2f70e1d921466db5f4afaac32d0
Nahla Gamal <nahladayya@gmail.com>
2023-06-01 13:54:45 +0300

    merge with register branch

--- hotelapp/src/app/app-routing.module.ts
+++ hotelapp/src/app/app-routing.module.ts
@@ -11,7 +11,10 @@ import { AuthgaurdsGuard } from './Gaurds/authgaurds.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: "full", },
-  { path: "rooms", loadChildren: () => import("./Components/room-resource/room-resource.module").then(m => m.RoomResourceModule) },  
+  // { path: "rooms", loadChildren: () => import("./Components/room-resource/room-resource.module").then(m => m.RoomResourceModule) },  
+  { path: "service", loadChildren: () => import("./Components/service/service.module").then(m => m.ServiceModule) },  
+  { path: "resource", loadChildren: () => import("./Components/resource/resource.module").then(m => m.ResourceModule) },  
+  // { path: "rooms", loadChildren: () => import("./Components/room-resource/room-resource.module").then(m => m.RoomResourceModule) , canActivate: [AuthgaurdsGuard]  },  
  
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
